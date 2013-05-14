bec4d41bab4e7b963df7d24a0d2caaefe5045f69
Ivan Stojic <ivans@ordecon.com>
2013-05-14 23:52:29 +0200

    almost found FIND

--- asm/forth-dictionary.s
+++ asm/forth-dictionary.s
@@ -13,20 +13,34 @@ actual_find:
     ldr r2, var_LATEST
 
 1:
-    cmp r2, #0
+    cmp r2, #0                  /* if we are at the end, well, go home... */
     bxeq lr
 
-    add r3, r2, #4
-    ldrb r4, [r3], #1
+    add r3, r2, #4              /* r3 is the length+flags field of the dict entry */
 
-    cmp r4, r0
-    bne 3f
+    ldrb r4, [r3], #1           /* load lenflags into r4, r3 is now looking at start of name */
+    mov r5, #F_NOIMM_MASK       /* strip possible immediate flag */
+    and r4, r5
 
+    cmp r4, r0                  /* length doesn't match, so get next word and iterate */
+    ldrne r2, [r2]
+    bne 1b
+
+    mov r5, r0                  /* set up the loop */
+    mov r6, r1
+2:
+    ldrb r7, [r3], #1           /* one from the dict */
+    ldrb r8, [r6], #1           /* one from the param */
+    cmp r7, r8
+    ldrne r2, [r2]              /* not the same, abort */
+    bne 1b
+
+    sub r5, #1
+    cmp r5, #0
+    bne 2b
 
 3:
-    
-
-    bx lr
+    bx lr                       /* if i got here, there's a result in r2, either 0x0 or a real ptr */
 
 
 defvar "HERE",4,,HERE,end_of_precompiled_code
--- asm/startup.s
+++ asm/startup.s
@@ -7,12 +7,19 @@ _entrypoint:
     ldr sp, =stack_top
     ldr r11, =return_stack_top;
 
+    mov r0, #6
+    ldr r1, msg
+    bl actual_find
+
     ldr r12, =dummy
     NEXT
 
     /* After everything's said and done... this will start the system */
     ldr r12, =start_over_here
     NEXT
+
+msg:
+    .string "LATEST"
 
 dummy:
     .int LIT, 'a', EMIT
