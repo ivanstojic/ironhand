3db0bfff79ff17a52375c90e776a35024a72e7df
Ivan Stojic <ivans@ordecon.com>
2013-05-15 15:26:34 +0200

    CREATE dictionary header word

--- asm/forth-dictionary.s
+++ asm/forth-dictionary.s
@@ -40,10 +40,65 @@ actual_find:
     bne 2b
 
 3:
-    bx lr                       /* if i got here, there's a result in r2, either 0x0 or a real ptr */
+    bx lr                       /* there's a result in r2, either 0x0 or a real ptr */
 
 
-defvar "HERE",4,,HERE,end_of_precompiled_code
+/* ( a-addr -- a-addr2 ) a-addr2 is the pointer to CFA of dict word at a-addr */
+defcode ">CFA",4,,TCFA
+    pop {r0}
+    bl actual_tcfa
+    push {r0}
+    NEXT
+
+actual_tcfa:
+    add r0, #4          /* skip link to previous entry */
+    ldrb r1, [r0], #1   /* pick up flag+len, move over that */
+    mov r2, #F_LENMASK
+    and r1, r2          /* strip flags off the length */
+
+    add r0, r2          /* move by length bytes forward */
+    add r0, #3          /* and balign 4 the address */
+    and r0, #~3
+
+    bx lr
+
+
+/* ( a-addr -- a-addr2 ) a-addr2 is the pointer to first DFA of dict word at a-addr */
+defword ">DFA",4,,TDFA
+    .int TCFA, INCR4, EXIT
+
+
+
+/* ( a-addr w -- ) create dict entry header of name at a-addr with len w, update HERE/LATEST */
+defcode "CREATE",6,,CREATE
+    pop {r0, r1} 
+    ldr r2, =var_HERE
+    push {r2}
+    ldr r3, =var_LATEST
+
+    str r3, [r2], #4        /* link to previous entry through var_LATEST */
+    strb r0, [r2], #1       /* word length + flags */
+    
+    mov r4, r0
+    mov r5, r1
+
+1:
+    ldrb r6, [r5], #1       /* copy name from the given buffer, to HERE */
+    strb r6, [r2], #1
+    sub r4, #1
+    cmp r4, #0
+    bne 1b                  /* repeat until we reach length */
+
+    add r2, #3
+    and r2, #~3
+    ldr r7, var_HERE
+    str r2, [r7]            /* set var_HERE from the r2 which was tracking data */
+
+    ldr r7, var_LATEST
+    pop {r2}
+    str r2, [r7]            /* set var_LATEST to wherever var_HERE pointed at the start */
+
+    NEXT
 
 /* making sure this stays at the bottom of this file means we don't have to
    manually track the last dictionary pointer.
@@ -52,6 +107,7 @@ defvar "HERE",4,,HERE,end_of_precompiled_code
    defvar is at the end, gas variable "link" will always point to
    the last def<whatever> for us
 */
+defvar "HERE",4,,HERE,end_of_precompiled_code
 defvar "LATEST",6,,LATEST,link
 
 /* Just a helpful label so objdump constants look prettier */
