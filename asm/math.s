9cb7bbda72c92caa2aef71967fa5df789d7b6339
Ivan Stojic <ivans@ordecon.com>
2013-05-22 22:41:36 +0200

    add native multiplication

--- asm/math.s
+++ asm/math.s
@@ -4,6 +4,12 @@ defcode "+",1,,ADD
     push {r0}
     NEXT
 
+defcode "*",1,,MUL
+    pop {r0, r1}
+    mul r0, r0, r1
+    push {r0}
+    NEXT
+
 defcode "4+",2,,INCR4
     pop {r0}
     add r0, #4
