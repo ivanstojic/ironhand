695ec62ff7c759e047bbd978389b11101a4e3ecd
Ivan Stojic <ivans@ordecon.com>
2013-08-29 14:40:58 +0200

    implement equality test (=)

--- asm/logic.s
+++ asm/logic.s
@@ -4,6 +4,21 @@ defcode "AND",3,,LAND
     push {r0}
     NEXT
 
+
+defcode "=",1,,EQU
+    pop {r0, r1}
+    cmp r0, r1
+    beq 1f
+
+    mov r0, #0
+    push {r0}
+    NEXT
+
+1:
+    mov r1, #1
+    push {r1}
+    NEXT
+
 defcode "0=",2,,ZEQU
     pop {r0}
     mov r1, #0
